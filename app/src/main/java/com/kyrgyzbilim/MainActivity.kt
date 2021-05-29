aaa11f3e28dd8714572ecbd97d8fe4f358444390
TilekSydykov <tilekfromkg@gmail.com>
2021-05-29 13:47:21 +0600

    sign_in and sign_up layout fixes

--- app/src/main/java/com/kyrgyzbilim/MainActivity.kt
+++ app/src/main/java/com/kyrgyzbilim/MainActivity.kt
@@ -1,18 +1,33 @@
 package com.kyrgyzbilim
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.kyrgyzbilim.data.UserData
+import com.kyrgyzbilim.ui.authorization.AuthActivity
+import com.kyrgyzbilim.ui.courses.CoursesFragmentDirections
 
 val TAG = "me"
 
 open class MainActivity : AppCompatActivity() {
-
-    private val fragmentManager: FragmentManager = supportFragmentManager
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // checking login
+        val token = UserData.of(this).getToken()
+
+        if(token == null){
+            // not logged in
+            startActivity(Intent(this, AuthActivity::class.java))
+            Thread{
+                Thread.sleep(500)
+                finish()
+            }.start()
+        }
     }
 }
--- app/src/main/java/com/kyrgyzbilim/ui/authorization/AuthActivity.kt
+++ app/src/main/java/com/kyrgyzbilim/ui/authorization/AuthActivity.kt
@@ -1,7 +1,10 @@
-package com.kyrgyzbilim
+package com.kyrgyzbilim.ui.authorization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.kyrgyzbilim.R
+import com.kyrgyzbilim.ui.courses.CoursesFragmentDirections
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
