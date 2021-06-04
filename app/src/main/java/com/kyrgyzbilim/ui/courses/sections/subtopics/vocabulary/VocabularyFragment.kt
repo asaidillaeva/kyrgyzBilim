578318f18c8b6a8957fb25b22dba5bf0d6b8e2f5
farida14ff <feriii1906@gmail.com>
2021-06-04 19:35:41 +0600

    try paly audio

--- app/src/main/java/com/kyrgyzbilim/ui/courses/sections/subtopics/vocabulary/VocabularyFragment.kt
+++ app/src/main/java/com/kyrgyzbilim/ui/courses/sections/subtopics/vocabulary/VocabularyFragment.kt
@@ -1,6 +1,5 @@
 package com.kyrgyzbilim.ui.courses.sections.subtopics.vocabulary
 
-import android.media.AudioManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
